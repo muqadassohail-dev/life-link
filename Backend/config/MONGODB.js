1e0e3d4b7dbc1ff232069435ef5a09b64b3c0da0
muqadassohail-dev <muqadass243@gmail.com.com>
2026-05-17 00:39:28 +0500

    first commit

--- Backend/config/MONGODB.js
+++ Backend/config/MONGODB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGOURL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('✅ MongoDB Connected Successfully');
     } catch (error) {
         console.error('❌ MongoDB Connection Error:', error.message);
