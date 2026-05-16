706ca56516b9145fcc2c0ab16e8719aef7c1f6a4
muqadassohail-dev <muqadass243@gmail.com.com>
2026-05-17 02:04:27 +0500

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization", "token"]
 }));
 
 // Handle preflight requests
